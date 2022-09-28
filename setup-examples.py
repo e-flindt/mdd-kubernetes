904025611e341adec92fbf6165c6856171dc980d
Eric Flindt <e.flindt@web.de>
2022-09-28 17:37:00 +0200

    Preparing for consumers

--- setup-examples.py
+++ setup-examples.py
@@ -1,12 +1,5 @@
 import sys, json, urllib3
 
-giteaAPI = 'http://infra-gitea-http.default.svc.cluster.local/api/v1'
-argoCDHook = ' http://argocd-server.argocd.svc.cluster.local/api/webhook'
-
-headers = urllib3.make_headers(basic_auth='gitea-admin:password')
-headers['Content-Type']='application/json'
-http = urllib3.PoolManager()
-
 def requestToJSON(request):
     return json.loads(request.data.decode('utf-8'))
 
@@ -36,14 +29,26 @@ class Repository:
             }
         }
 
+giteaAPI = 'http://infra-gitea-http.default.svc.cluster.local/api/v1'
+argoCDHook = 'http://argocd-server.argocd.svc.cluster.local/api/webhook'
+mddWebhook = 'http://on-change-webhook-eventsource-svc.mdd.svc.cluster.local/onchange'
+
+headers = urllib3.make_headers(basic_auth='gitea-admin:password')
+headers['Content-Type']='application/json'
+http = urllib3.PoolManager()
+
 repos = [
-    Repository('microservice', 'A metamodel for microservices', ['version-microservice-0', 'instanceof-ecore-0'], argoCDHook),
-    Repository('customer-microservice', 'A model for a customer microservice', ['version-customer-ms-0', 'instanceof-microservice-0'], argoCDHook),
-    Repository('shopping-cart-microservice', 'A model for a shopping cart microservice', ['version-shopping-cart-ms-0', 'instanceof-microservice-0'], argoCDHook),
-    Repository('order-microservice', 'A model for an order microservice', ['version-order-ms-0', 'instanceof-microservice-0'], argoCDHook),
-    Repository('microservice-spring-boot', 'A generator for microservices to create Spring Boot applications', ['version-ms-spring-boot-0', 'instanceof-trafomm-0', 'input-microservice-0'], argoCDHook),
-    Repository('microservice-dot-net', 'A generator for microservices to create Dot Net applications', ['version-ms-dot-net-0', 'instanceof-trafomm-0', 'input-microservice-0'], argoCDHook),
-    Repository('microservice-python', 'A generator for microservices to create Python applications', ['version-ms-python-0', 'instanceof-trafomm-0', 'input-microservice-0'], argoCDHook)
+    Repository('microservice', 'A metamodel for microservices', ['version-microservice-0', 'instanceof-ecore-0'], mddWebhook),
+    Repository('microservice-validator', 'A validator for microservices', ['version-ms-validator-0', 'input-microservice-0'], mddWebhook),
+    Repository('microservice-analyzer', 'An analyzer for microservices', ['version-ms-analyzer-0', 'input-microservice-0'], mddWebhook),
+    Repository('microservice-simulator', 'A simulator for microservices', ['version-ms-simulator-0', 'input-microservice-0'], mddWebhook),
+    Repository('customer-microservice', 'A model for a customer microservice', ['version-customer-ms-0', 'instanceof-microservice-0'], mddWebhook),
+    Repository('shopping-cart-microservice', 'A model for a shopping cart microservice', ['version-shopping-cart-ms-0', 'instanceof-microservice-0'], mddWebhook),
+    Repository('order-microservice', 'A model for an order microservice', ['version-order-ms-0', 'instanceof-microservice-0'], mddWebhook),
+    Repository('microservice-spring-boot', 'A generator for microservices to create Spring Boot applications', ['version-ms-spring-boot-0', 'instanceof-trafomm-0', 'input-microservice-0', 'output-spring-boot-0'], mddWebhook),
+    Repository('microservice-dot-net', 'A generator for microservices to create Dot Net applications', ['version-ms-dot-net-0', 'instanceof-trafomm-0', 'input-microservice-0', 'output-dot-net-0'], mddWebhook),
+    Repository('microservice-python', 'A generator for microservices to create Python applications', ['version-ms-python-0', 'instanceof-trafomm-0', 'input-microservice-0', 'output-python-0'], mddWebhook),
+    Repository('mdd-kubernetes', 'A platform to run mdd jobs on kubernetes', [], argoCDHook)
 ]
 
 for repo in repos:
